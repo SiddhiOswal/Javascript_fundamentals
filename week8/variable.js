f4892427a98d6ec48c4a29de7679a14aeea8ca11
SiddhiOswal <siddhioswal05@gmail.com>
2025-07-22 09:09:26 +0530

    Beginning with Javascript fundamentals

---
+++ week8/variable.js
@@ -0,0 +1,74 @@
+// 21/07/25
+
+// var num = 10;  //declaration
+// num=30
+// console.log("before block scope",num);
+
+// {
+//     var num = 20;    //redeclare
+//     console.log("inside block scope",num);
+// }
+
+// console.log("after block scope",num);
+
+// let number = 10;  //declaration
+//     number =20
+
+// console.log("before block scope",number);
+
+// {
+//     let number = 30;    //redeclare
+//     console.log("inside block scope",number);
+// }
+
+// console.log("after block scope",number);
+
+
+
+// const number = 10;  //declaration
+
+// console.log("before block scope",number);
+
+// {
+//     let number = 30;    //redeclare
+//     console.log("inside block scope",number);
+// }
+
+// console.log("after block scope",number);
+                       
+
+// let user = "Siddhi";
+// let age = 29;
+
+// let user - "Siddhi", age = 29;
+
+ 
+// let num = 9999999n;
+// console.log(num)
+
+// //typeof
+// console.log(Number.MAX_SAFE_INTEGER);      //maximum range of integer numbers after that those numbers are bigint
+
+
+// let num = BigInt("6666666666666666666666666666");
+// console.log(num)
+// console.log(typeof num);
+      
+const user = {
+    userName : "Sid",
+    age : 13,
+    isLoggedIn : true
+}
+
+console.log(user);
+console.log(user.age); //or
+console.log(user["age"]);
+
+//null
+// let acTemp = null;
+// console.log(acTemp);
+// console.log(typeof acTemp);
+
+let num;
+console.log(num)
+console.log(typeof num);
